62dc3a3085210e7b3a2555088f5090cc2449ac08
GrayWolf <gray.wolf@wolf-itechnologies.com>
2014-01-10 20:28:00 +0100

    Updated documentation.

--- library/src/main/java/com/wit/android/fragment/ActionBarFragment.java
+++ library/src/main/java/com/wit/android/fragment/ActionBarFragment.java
@@ -1,6 +1,6 @@
 /*
  * =================================================================================
- * Copyright (C) 2013 Martin Albedinsky [Wolf-ITechnologies]
+ * Copyright (C) 2013 - 2014 Martin Albedinsky [Wolf-ITechnologies]
  * =================================================================================
  * Licensed under the Apache License, Version 2.0 or later (further "License" only);
  * ---------------------------------------------------------------------------------
@@ -20,10 +20,19 @@
  */
 package com.wit.android.fragment;
 
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.util.AndroidRuntimeException;
+
 /**
  * <h4>Class Overview</h4>
  * <p>
  * Description.
+ * </p>
+ * <p>
+ * <b>Note, that implementation of this fragment can be used only within the context of
+ * {@link android.support.v7.app.ActionBarActivity}</b>.
  * </p>
  *
  * @author Martin Albedinsky
@@ -37,7 +46,7 @@ public class ActionBarFragment extends BaseFragment {
 	/**
 	 * Log TAG.
 	 */
-	private static final String TAG = ActionBarFragment.class.getSimpleName();
+	// private static final String TAG = ActionBarFragment.class.getSimpleName();
 
 	/**
 	 * Flag indicating whether the debug output trough log-cat is enabled or not.
@@ -86,12 +95,147 @@ public class ActionBarFragment extends BaseFragment {
 	 */
 
 	/**
+	 * @throws android.util.AndroidRuntimeException If the currently created parent activity isn't
+	 * instance of {@link android.support.v7.app.ActionBarActivity}.
+	 */
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		// Check if this fragment instance is placed within ActionBarActivity context.
+		if (!(getActivity() instanceof ActionBarActivity)) {
+			throw new AndroidRuntimeException(
+					"ActionBarFragment implementation can be used only within the context of ActionBarActivity."
+			);
+		}
+	}
+
+	/**
+	 * <p>
+	 * Same as {@link android.support.v7.app.ActionBarActivity#supportInvalidateOptionsMenu()}.
+	 * </p>
+	 *
+	 * @see #isActionBarActivityAvailable()
+	 */
+	public void invalidateOptionsMenu() {
+		if (isActionBarActivityAvailable()) {
+			getActionBarActivity().supportInvalidateOptionsMenu();
+		}
+	}
+
+	/**
+	 * <p>
+	 * Same as {@link android.support.v7.app.ActionBarActivity#supportRequestWindowFeature(int)}.
+	 * </p>
+	 *
+	 * @see #getActionBar()
+	 * @see #isActionBarActivityAvailable()
+	 */
+	public boolean requestWindowFeature(int featureID) {
+		return isActionBarActivityAvailable() && getActionBarActivity().supportRequestWindowFeature(featureID);
+	}
+
+	/**
 	 * Getters + Setters ---------------------
 	 */
 
 	/**
+	 * <p>
+	 * Same as {@link android.support.v7.app.ActionBarActivity#getSupportActionBar()}
+	 * </p>
+	 *
+	 * @see #requestWindowFeature(int)
+	 * @see #isActionBarActivityAvailable()
+	 * @see #getActionBarActivity()
+	 */
+	public ActionBar getActionBar() {
+		return isActionBarActivityAvailable() ? getActionBarActivity().getSupportActionBar() : null;
+	}
+
+	/**
+	 * <p>
+	 * Same as {@link android.support.v7.app.ActionBarActivity#setSupportProgress(int)}.
+	 * </p>
+	 *
+	 * @see #setProgressBarVisibility(boolean)
+	 * @see #isActionBarActivityAvailable()
+	 */
+	public void setProgress(int progress) {
+		if (isActionBarActivityAvailable()) {
+			getActionBarActivity().setSupportProgress(progress);
+		}
+	}
+
+	/**
+	 * <p>
+	 * Same as {@link android.support.v7.app.ActionBarActivity#setSupportProgressBarVisibility(boolean)}.
+	 * </p>
+	 *
+	 * @see #setProgress(int)
+	 * @see #setProgressBarIndeterminate(boolean)
+	 * @see #isActionBarActivityAvailable()
+	 */
+	public void setProgressBarVisibility(boolean visible) {
+		if (isActionBarActivityAvailable()) {
+			getActionBarActivity().setSupportProgressBarVisibility(visible);
+		}
+	}
+
+	/**
+	 * <p>
+	 * Same as {@link android.support.v7.app.ActionBarActivity#setSupportProgressBarIndeterminate(boolean)}.
+	 * </p>
+	 *
+	 * @see #setProgressBarIndeterminateVisibility(boolean)
+	 * @see #isActionBarActivityAvailable()
+	 */
+	public void setProgressBarIndeterminate(boolean indeterminate) {
+		if (isActionBarActivityAvailable()) {
+			getActionBarActivity().setSupportProgressBarIndeterminate(indeterminate);
+		}
+	}
+
+	/**
+	 * <p>
+	 * Same as {@link android.support.v7.app.ActionBarActivity#setSupportProgressBarIndeterminateVisibility(boolean)}.
+	 * </p>
+	 *
+	 * @see #setProgressBarIndeterminate(boolean)
+	 * @see #isActionBarActivityAvailable()
+	 */
+	public void setProgressBarIndeterminateVisibility(boolean visible) {
+		if (isActionBarActivityAvailable()) {
+			getActionBarActivity().setSupportProgressBarIndeterminateVisibility(visible);
+		}
+	}
+
+	/**
 	 * Protected -----------------------------
 	 */
+
+	/**
+	 * <p>
+	 * Returns flag indicating whether the parent ActionBarActivity of this fragment instance is
+	 * available or not.
+	 * </p>
+	 *
+	 * @return <code>True</code> if activity is available, <code>false</code> otherwise.
+	 * @see #getActionBarActivity()
+	 */
+	protected boolean isActionBarActivityAvailable() {
+		return getActionBarActivity() != null;
+	}
+
+	/**
+	 * <p>
+	 * Returns an instance of {@link android.support.v7.app.ActionBarActivity}.
+	 * </p>
+	 *
+	 * @return The current parent activity of this fragment instance casted to the ActionBarActivity.
+	 * @see #isActionBarActivityAvailable()
+	 */
+	protected ActionBarActivity getActionBarActivity() {
+		return ((ActionBarActivity) getActivity());
+	}
 
 	/**
 	 * Private -------------------------------
