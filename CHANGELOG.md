f1596f3b595f15e1b9cea4249a5448ea4313f856
Gray Wolfe <gray.wolf@wolf-itechnologies.com>
2014-09-07 11:12:46 +0200

    Added BackPress + ViewClick interfaces.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,9 @@ Change-Log
 
 > **RELEASE VERSION** (<i>RELEASE DATE</i>)
 
+### **4.0.5** (<i>**.09.2014</i>) ###
+- Added interfaces for BackPress + ViewClick events for fragment.
+
 ### **4.0.4** (<i>07.09.2014</i>) ###
 
 ### **4.0.3** (<i>05.09.2014</i>) ###
---
+++ library/src/main/java/com/wit/android/support/fragment/ViewClickWatcher.java
@@ -0,0 +1,43 @@
+/*
+ * =================================================================================================
+ *                    Copyright (C) 2014 Martin Albedinsky [Wolf-ITechnologies]
+ * =================================================================================================
+ *         Licensed under the Apache License, Version 2.0 or later (further "License" only).
+ * -------------------------------------------------------------------------------------------------
+ * You may use this file only in compliance with the License. More details and copy of this License 
+ * you may obtain at
+ * 
+ * 		http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * You can redistribute, modify or publish any part of the code written within this file but as it 
+ * is described in the License, the software distributed under the License is distributed on an 
+ * "AS IS" BASIS, WITHOUT WARRANTIES or CONDITIONS OF ANY KIND.
+ * 
+ * See the License for the specific language governing permissions and limitations under the License.
+ * =================================================================================================
+ */
+package com.wit.android.support.fragment;
+
+import android.view.View;
+
+/**
+ * <h4>Interface Overview</h4>
+ * <p>
+ * todo: description
+ * </p>
+ *
+ * @author Martin Albedinsky
+ */
+public interface ViewClickWatcher {
+
+	/**
+	 * <p>
+	 * Called to dispatch a view click event to this watcher instance.
+	 * </p>
+	 *
+	 * @param view The view which was clicked.
+	 * @return <code>True</code> if this watcher processed the click event for the specified <var>view</var>,
+	 * <code>false</code> otherwise.
+	 */
+	public boolean dispatchViewClick(View view);
+}
